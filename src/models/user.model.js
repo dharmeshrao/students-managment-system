ab22974cc0dda08ca159820269492a8ac26e8b58
Dharmesh <yadavdydv@gmail.com>
2021-12-29 00:17:56 +0530

    update

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,20 +1,20 @@
-const bcryptjs = require('bcryptjs')
+const bcryptjs = require("bcryptjs");
 const mongoose = require("mongoose");
 const userSchema = mongoose.Schema({
-  email: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true, minLength: 8 },
   password: { type: String, required: true },
 });
 
-userSchema.pre("save",function(next){
-  if(!this.isModified("password"))return next()
-  const hash = bcryptjs.hashSync(this.password,8)
+userSchema.pre("save", function (next) {
+  if (!this.isModified("password")) return next();
+  const hash = bcryptjs.hashSync(this.password, 8);
   this.password = hash;
-  return next()
-})
-userSchema.methods.checkPassword = function(password){
-  const match = bcryptjs.compareSync(password,this.password)
-  return match
-}
+  return next();
+});
+userSchema.methods.checkPassword = function (password) {
+  const match = bcryptjs.compareSync(password, this.password);
+  return match;
+};
 
 const User = mongoose.model("user", userSchema);
 module.exports = User;
